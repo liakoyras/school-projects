5dfbe470ba2ae8038389644f1d9394f5af66264f
Ilias Chanis <iliashanis@gmail.com>
2019-11-18 20:22:48 +0200

    Integrated Circuits-L3: Created tictactoe module. Changed isFull calculation.

--- Integrated Circuits/Lab 3/full.sv
+++ Integrated Circuits/Lab 3/full.sv
@@ -4,14 +4,16 @@ input logic [8:0] O,
 output logic isFull
 );
 
-logic temp;
+logic temp[8:0];
 assign temp = X || O;
 
-always_comb
+assign isFull = &temp;
+
+/* always_comb
 begin
 	if(temp == 9’b111111111)
 		isFull = 1;
 	else
 		isFull = 0;
-end
+end */
 endmodule
--- Integrated Circuits/Lab 3/tictactoe.sv
+++ Integrated Circuits/Lab 3/tictactoe.sv
@@ -8,4 +8,11 @@ output logic winO,
 output logic noWin
 );
 
+error e (.X[8:0](x[8:0]), .O[8:0](o[8:0]), .err(error));
+full f (.X[8:0](x[8:0]), .O[8:0](o[8:0]), .isFull(full));
+winX w0 (.X[8:0](x[8:0]), .win(winX));
+winO w1 (.O[8:0](o[8:0]), .win(winO));
 
+asign noWin = full && ~WinX && ~win0;
+
+endmodule
