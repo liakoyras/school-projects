27395d9636239fb6734975ce32d57eca5d3c6013
Ilias Chanis <iliashanis@gmail.com>
2019-12-16 22:19:50 +0200

    Integrated Circuits-L5: Fixed the 7 segment ecnoder to match or fpga. Added reset to the priority register.

--- Integrated Circuits/Lab 5/7_segment_decoder.sv
+++ Integrated Circuits/Lab 5/7_segment_decoder.sv
@@ -13,15 +13,7 @@ always_comb begin
 		3'b110: out_7_bits = 7'b1111101;
 		3'b111: out_7_bits = 7'b0000111;
 		default: out_7_bits = 7'b0000000;
-		/* 3'b000: out_7_bits = 7'b1000000;
-		3'b001: out_7_bits = 7'b1111001;
-		3'b010: out_7_bits = 7'b0100100;
-		3'b011: out_7_bits = 7'b0110000;
-		3'b100: out_7_bits = 7'b0011001;
-		3'b101: out_7_bits = 7'b0010010;
-		3'b110: out_7_bits = 7'b0000010;
-		3'b111: out_7_bits = 7'b1111000;
-		default: out_7_bits = 7'b1111111; */
 	endcase
+	out_7_bits = ~out_7_bits;
 end
 endmodule
--- Integrated Circuits/Lab 5/priority_register.sv
+++ Integrated Circuits/Lab 5/priority_register.sv
@@ -1,5 +1,6 @@
 module priority_register(
 	input logic clock,
+	input logic reset,
 	input logic [2:0] d_i,
 	input logic en_i,
 	output logic [2:0] q_o
@@ -9,12 +10,13 @@ logic [2:0] register;
 
 assign q_o = register;
 
-always_ff @(posedge clock) begin
-
-	if(en_i) begin
-		register <= d_i;
+always_ff @(posedge clock, posedge reset) begin
+	if(reset) register <= 3'b111;
+	else begin 
+		if(en_i) begin
+			register <= d_i;
+		end
 	end
-
 end
 
 endmodule
